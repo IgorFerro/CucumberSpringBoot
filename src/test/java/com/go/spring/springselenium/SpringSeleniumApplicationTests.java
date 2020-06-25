e7be69141dda8dcfd30c60984ff4409ba4f69c09
IgorFerro <32639782+IgorFerro@users.noreply.github.com>
2020-06-25 10:53:51 -0300

    project structure

---
+++ src/test/java/com/go/spring/springselenium/SpringSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go.spring.springselenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
