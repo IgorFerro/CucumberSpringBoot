e13e8b784c2f19707d95fdaf2ee8a08a9ca436d0
igorferro <igor.ferro.parreira@gmail.com>
2020-06-26 09:22:56 -0300

    Value Injection Using Setter Methods

--- src/test/java/com/go/spring/springselenium/Address.java
+++ src/test/java/com/go/spring/springselenium/Address.java
@@ -1,14 +1,18 @@
 package com.go.spring.springselenium;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Address {
+	
+	@Value("${street}")
 	private String street;
 	
-	public Address() {
+	
+	/*public Address() {
 		this.street = "Rua 10";
-	}
+	}*/
 
 	public String getStreet() {
 		return street;
