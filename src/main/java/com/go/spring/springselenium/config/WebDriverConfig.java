6b358a64c420e7356292e6276fd6d52b075d4b0f
igorferro <igor.ferro.parreira@gmail.com>
2020-06-27 07:18:11 -0300

    Base Class

--- src/main/java/com/go/spring/springselenium/config/WebDriverConfig.java
+++ src/main/java/com/go/spring/springselenium/config/WebDriverConfig.java
@@ -2,6 +2,8 @@ package com.go.spring.springselenium.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 @Configuration
 public class WebDriverConfig {
 	
+	@Value("${default.timeout:30}")
+	private int timeout;
+	
 	@Bean
 	public WebDriver chromeDriver() {
 		WebDriverManager.chromedriver().version("83.0.4103.39");
 		return new ChromeDriver();
+	}
+	
+	@Bean
+	public WebDriverWait webdriverWait(WebDriver driver) {
+		return new WebDriverWait(driver, this.timeout);
 	}
 	
 	
---
+++ src/main/java/com/go/spring/springselenium/page/Base.java
@@ -0,0 +1,24 @@
+package com.go.spring.springselenium.page;
+
+import javax.annotation.PostConstruct;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class Base {
+	
+	@Autowired
+	protected WebDriver driver;
+	
+	@Autowired
+	protected WebDriver wait;
+	
+	@PostConstruct
+	private void init() {
+		PageFactory.initElements(this.driver, this);
+	}
+	
+	public abstract boolean isAt();
+
+}
