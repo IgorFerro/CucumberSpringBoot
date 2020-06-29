60d8dbce6f2225a930d5318b21312169d44ac5c2
igorferro <igor.ferro.parreira@gmail.com>
2020-06-29 08:13:25 -0300

    Google Page Object

--- src/main/java/com/go/spring/springselenium/page/Base.java
+++ src/main/java/com/go/spring/springselenium/page/Base.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
 public abstract class Base {
 	
 	@Autowired
--- src/main/java/com/go/spring/springselenium/page/google/SearchComponent.java
+++ src/main/java/com/go/spring/springselenium/page/google/SearchComponent.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
 
 import com.go.spring.springselenium.page.Base;
 
+@Component
 public class SearchComponent extends Base {
 	
 	@FindBy(name = "q")
