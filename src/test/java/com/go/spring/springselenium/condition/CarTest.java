bebf3f9a6b291dfb30024ac2e85a0592bf9eb64c
igor.ferro.parreira@gmail.com <igor.ferro.parreira@gmail.com>
2020-06-30 12:59:57 -0300

    Injecting Unique Bean - @Qualifier

--- src/test/java/com/go/spring/springselenium/condition/CarTest.java
+++ src/test/java/com/go/spring/springselenium/condition/CarTest.java
@@ -1,18 +1,20 @@
 package com.go.spring.springselenium.condition;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.testng.annotations.Test;
 
 import com.go.spring.springselenium.SpringBaseTestNGTest;
 
 public class CarTest extends SpringBaseTestNGTest {
 
+	@Qualifier("accord")
 	@Autowired
 	private Car car;
 	
 	@Test
 	public void carTest() {
-		
+		this.car.run();
 	}
 	
 }
