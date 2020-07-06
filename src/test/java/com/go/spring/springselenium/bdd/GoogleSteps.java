a968367ca1c2bc21c35a1cca24ecb72ec37a0042
igor.ferro.parreira@gmail.com <igor.ferro.parreira@gmail.com>
2020-07-06 10:15:42 -0300

    Steps Definition Creation For Google Test - Part 1

---
+++ src/test/java/com/go/spring/springselenium/bdd/GoogleSteps.java
@@ -0,0 +1,33 @@
+package com.go.spring.springselenium.bdd;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSteps {
+	
+	@Given("I am on the google site")
+	public void i_am_on_the_google_site() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter {string} as a keyword")
+	public void i_enter_as_a_keyword(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I click on the search button")
+	public void i_click_on_the_search_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I should see al least {string} results")
+	public void i_should_see_al_least_results(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+
+ }
+}
--- src/test/java/features/search.feature
+++ src/test/java/features/search.feature
@@ -1,15 +1,13 @@
-Feature:
+Feature: 
 
-Scenario Outline: I want to search on google site
-   Given I am on the google site
-   When I enter "<keyword>" as a keyword
-   And I click on the search button
-   Then I should see al least "<count>" results
-   
-   Examples:
-   | keyword   | count   |
-   | selenium  | 2       |
-   | java      | 5       |
-   | spring    | 7       |
-   
-   
+  Scenario Outline: I want to search on google site
+    Given I am on the google site
+    When I enter "<keyword>" as a keyword
+    And I click on the search button
+    Then I should see al least "<count>" results
+
+    Examples: 
+      | keyword  | count |
+      | selenium |     2 |
+      | java     |     5 |
+      | spring   |     7 |
